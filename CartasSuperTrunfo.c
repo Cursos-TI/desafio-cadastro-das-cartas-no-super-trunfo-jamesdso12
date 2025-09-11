b600aa86f3b7ef9245835f7d092328a781a661f0
programaçao c <jamesdouglasdso@gmail.com>
2025-09-11 13:29:40 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,45 +1,78 @@
 #include <stdio.h>
 
 int main() {
-    char carta[20] = "1";          
-    char codigo[20] = "a01";             
-    char estado[20] = "Amazonas";       
-    char cidade[20] = "Sucubduri";       
-    double populacao = 380000;          
-    float areakm = 20606.000;           
-    float pib = 25086.000000;              
-    int pontosturisticos = 20;         
+    
+    char carta[20];       
+    char codigo[20];          
+    char estado[20];     
+    char cidade[20];    
+    double populacao;     
+    float areakm;           
+    float pib;            
+    int pontosturisticos; 
 
-    printf("Carta: %s\n", carta);
-    printf("Codigo: %s\n", codigo);
-    printf("Estado: %s\n", estado);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %.0f\n", populacao);
-    printf("Área km²: %.3f\n", areakm);
-    printf("PIB: %.3f\n", pib);
-    printf("Pontos Turísticos: %d\n", pontosturisticos);
+    
 
-printf("\n");
+      printf("======= carta ======== \n");
+      printf("digite sua carta:");
+      scanf("%s", &carta);
+      printf("digite o codigo da sua carta: \n");
+      scanf("%s", &codigo);
+      printf("Digite seu estado: \n");
+      scanf("%s", &estado);
+      printf("Digite sua cidade: \n");
+      scanf("%s", &cidade );
+      printf("Digite a pupulaçao: \n");
+      scanf("%f", &populacao);
+      printf("Digite a area em km: \n");
+      scanf("%f", &areakm);
+      printf("Digite o PIB: \n");
+      scanf("%f", &pib);
+      printf("Digite quantidae de pontos turisticos de sua cidade:\n");
+      scanf("%d", &pontosturisticos);
+
+
+   char carta1[20];
+   char codigo1[20];
+   char estado1[20];
+   char cidade1[20];
+   double populacao1;
+   float areakm1;
+   float pib1;
+   int pontosturisticos1;
+     
+      printf("======= carta1 ======== \n");
 
    
-    char carta1[20] = "2";
-    char codigo1[20] = "b01";
-    char estado1[20] = "Mato Grosso";
-    char cidade1[20] = "Gafanhotos";
-    double populacao1 = 45000.000000;  
-    float areakm1 = 600.0000000;
-    float pib1 = 750.000000;
-    int pontosturisticos1 = 35;
+      
+      printf("digite sua carta: \n");
+      scanf("%s", &carta1);
+      printf("digite o codigo da sua carta: \n");
+      scanf("%s", &codigo1);
+      printf("Digite seu estado: \n");
+      scanf("%s", &estado1);
+      printf("Digite sua cidade: \n");
+      scanf("%s", &cidade1 );
+      printf("Digite a pupulaçao: \n");
+      scanf("%f", populacao1 );
+      printf("Digite a area em km: \n");
+      scanf("%f", &areakm1);
+      printf("Digite o PIB: \n");
+      scanf("%f", &pib1);
+      printf("Digite quantidae de pontos turisticos de sua cidade: \n");
+      scanf("%d", &pontosturisticos1);
 
-    // Exemplo de impressão:
-    printf("Carta: %s\n", carta1);
-    printf("Codigo: %s\n", codigo1);
-    printf("Estado: %s\n", estado1);
-    printf("Cidade: %s\n", cidade1);
-    printf("Populacao: %.2f\n", populacao1);
-    printf("Area (km²): %.2f\n", areakm1);
-    printf("PIB: %.2f\n", pib1);
-    printf("Pontos Turisticos: %d\n", pontosturisticos1);
+ if ( populacao > populacao1)
+ {
+  printf(" carta venceu ");
+ } else  {
+   printf("carta1 venceu");
+ }
+ 
 
-    return 0;
+
+
+
+
+
 }
